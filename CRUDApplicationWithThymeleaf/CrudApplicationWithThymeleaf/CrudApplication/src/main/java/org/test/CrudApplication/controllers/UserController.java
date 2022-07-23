fd1cdf864a66a366045f4bc49c30db1b88d337dc
Vijey-A <106647684+Vijey-A@users.noreply.github.com>
2022-07-23 16:32:34 +0530

    Uploaded

--- CRUDApplicationWithThymeleaf/CrudApplicationWithThymeleaf/CrudApplication/src/main/java/org/test/CrudApplication/controllers/UserController.java
+++ CRUDApplicationWithThymeleaf/CrudApplicationWithThymeleaf/CrudApplication/src/main/java/org/test/CrudApplication/controllers/UserController.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserRepository userRepository;
-
+    
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -37,36 +37,30 @@ public class UserController {
         if (result.hasErrors()) {
             return "add-user";
         }
-
         userRepository.save(user);
         return "redirect:/index";
     }
 
     @GetMapping("/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         model.addAttribute("user", user);
         return "update-user";
     }
 
     @PostMapping("/update/{id}")
-    public String updateUser(@PathVariable("id") long id, @Valid User user,
-                             BindingResult result, Model model) {
+    public String updateUser(@PathVariable("id") long id, @Valid User user,BindingResult result, Model model) {
         if (result.hasErrors()) {
             user.setId(id);
             return "update-user";
         }
-
         userRepository.save(user);
         return "redirect:/index";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") long id, Model model) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
         return "redirect:/index";
     }
